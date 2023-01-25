14971a02f1fe6118df3cd574e8ddb04aefb78eca
Gururaj Deshpande <66385690+guru-desh@users.noreply.github.com>
2023-01-24 22:10:46 -0500

    edit 1.5 second subclip and add ffmpeg command

--- run_decode.sh
+++ run_decode.sh
@@ -3,4 +3,4 @@
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/cuda-10.2/compat 
 cd /Mobile-Data-Processing-Pipeline
 python3 create_batches.py --num_batches 40
-python3 decode.py --job_array_num ${PBS_ARRAYID} --backup_dir /data/sign_language_videos/raw --dest_dir /data/sign_language_videos/split --num_threads 24
+python3 decode.py --job_array_num ${PBS_ARRAYID} --ffmpeg_loglevel info --backup_dir /data/sign_language_videos/raw --dest_dir /data/sign_language_videos/split --num_threads 24
